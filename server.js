5c11dd053afe56b77b7f7c4c99d83affc58c90b8
Okoye Moses <kingsmanmoses@gmail.com>
2023-10-05 20:18:48 +0100

    index

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.use((err, req, res, next) => {
 });
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.resolve(__dirname, '../e-lib/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 })
 
 // To listen for a port/ request number
